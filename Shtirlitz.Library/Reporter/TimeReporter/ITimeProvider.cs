7c4a0f6fbc5a7c1d2d0f1cf26d94365459f835bd
Anton Korzhenevskiy <ak@korjsoft.ru>
2014-01-15 20:25:03 +0400

    Added new time reporter

---
+++ Shtirlitz.Library/Reporter/TimeReporter/ITimeProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shtirlitz.Reporter
+{
+	public interface ITimeProvider
+	{
+		DateTimeOffset GetTime();
+	}
+}
