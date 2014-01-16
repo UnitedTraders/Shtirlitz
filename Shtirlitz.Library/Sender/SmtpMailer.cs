51c3a7f3b7fef8fecebf2b8be201295a8574f834
alex_programmer <achuksin@mail.ru>
2014-01-16 22:51:36 +0400

    Added SmtpMailer

---
+++ Shtirlitz.Library/Sender/SmtpMailer.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading;
+//using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using Shtirlitz.Common;
+
+namespace Shtirlitz.Sender
+{
+    /// <summary>
+    /// Sends email via SMTP
+    /// </summary>
+    public class SmtpMailer : ISender
+    {
+        private SmtpClient smtp;
+        private MailMessage message;
+        private MailInfo mailInfo;
+
+        public SmtpMailer(MailInfo mailInfo)
+        {
+            this.mailInfo = mailInfo;
+        }
+
+        public void Send(string archiveFilename, CancellationToken cancellationToken, SimpleProgressCallback progressCallback = null)
+        {
+            InitializeSmtp();
+            CreateMessage();
+            AttachFile(archiveFilename);
+            smtp.Send(message);
+            message.Dispose();
+        }
+
+        /// <summary>
+        /// Log in to the SMTP server
+        /// </summary>
+        private void InitializeSmtp()
+        {
+            smtp = new SmtpClient(mailInfo.Host, mailInfo.Port);
+            smtp.Credentials = new NetworkCredential(mailInfo.Login, mailInfo.Password);
+        }
+
+        /// <summary>
+        /// Create a message
+        /// </summary>
+        private void CreateMessage()
+        {
+            message = new MailMessage();
+            message.From = new MailAddress(mailInfo.Login);
+            message.To.Add(new MailAddress(mailInfo.EmailTo));
+            message.Subject = mailInfo.Subject;
+            message.Body = mailInfo.Body;
+        }
+
+        /// <summary>
+        /// Attach a file
+        /// </summary>
+        private void AttachFile(string attachFileName)
+        {
+            Attachment attach = new Attachment(attachFileName, MediaTypeNames.Application.Octet);
+            // Adding information to a file
+            ContentDisposition disposition = attach.ContentDisposition;
+            disposition.CreationDate = System.IO.File.GetCreationTime(attachFileName);
+            disposition.ModificationDate = System.IO.File.GetLastWriteTime(attachFileName);
+            disposition.ReadDate = System.IO.File.GetLastAccessTime(attachFileName);
+            message.Attachments.Add(attach);
+        }
+    }
+}
---
+++ Shtirlitz.Tests/SmtpMailerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Shtirlitz.Sender;
+using Xunit;
+
+namespace Shtirlitz.Tests
+{
+    public class SmtpMailerTests : ShtirlitzBaseTestClass
+    {
+        private static MailInfo mailInfo = new MailInfo();
+
+        public SmtpMailerTests()
+            : this(new SmtpMailer(mailInfo))
+        {
+            mailInfo.Subject = "Hi!";
+            mailInfo.Body = "bla bla bla";
+            mailInfo.EmailTo = "test@gmail.com";
+            mailInfo.Host = "smtp.mail.ru";
+            mailInfo.Port = 25;
+            mailInfo.Login = "test@mail.ru";
+            mailInfo.Password = "*****";
+
+        }
+
+        public SmtpMailerTests(SmtpMailer smtpMailer)
+            : base (null, new List<ISender> { smtpMailer })
+        { }
+
+        [Fact]
+        public void SendTheArchiveFile()
+        {
+            // act and assert
+            Assert.DoesNotThrow(() => RunSynchronously(true));
+        }
+    }
+}
